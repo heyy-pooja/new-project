b6e111fa390eaf08fa699d2868408e6a19a2e31b
pooja <pooja@gamil.com>
2024-07-21 23:27:40 +0530

    server setup

---
+++ routes/auth.routes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router()
+const authController = require("./../controller/auth.controller")
+
+router.post("/register-admin", authController.registerAdmin)
+    .post("/login-admin", authController.loginAdmin)
+    .post("/verify-admin-otp", authController.verifyOTP)
+    .post("/logout-admin", authController.logoutAdmin)
+
+module.exports = router
